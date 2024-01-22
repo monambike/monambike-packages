89cf522a6648fa7936b0593d4a3434d3c338712e
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2024-01-22 15:07:40 -0300

    Initial Commit

---
+++ src/GithubProjects.cs
@@ -0,0 +1,40 @@
+﻿using KWIJisho.Models.Apis;
+using Newtonsoft.Json;
+
+namespace Monambike.Data
+{
+    public class GithubProjects
+    {
+        /// <summary>
+        /// HttpClient for making api requests.
+        /// </summary>
+        readonly static HttpClient HttpClient = new() { BaseAddress = new Uri("https://api.github.com/repos/") };
+
+        internal static async Task<GithubResponse> Get()
+        {
+            var response = await HttpService.GetAsync(HttpClient, $"monambike");
+            return JsonConvert.DeserializeObject<GithubResponse>(response);
+        }
+
+        internal class GithubResponse
+        {
+            [JsonProperty("name")]
+            internal string Name { get; set; }
+
+            [JsonProperty("private")]
+            internal bool Private { get; set; }
+
+            [JsonProperty("html_url")]
+            internal string Url { get; set; }
+
+            [JsonProperty("language")]
+            internal string Language { get; set; }
+
+            [JsonProperty("created_at")]
+            internal string CreateDate { get; set; }
+
+            [JsonProperty("pushed_at")]
+            internal string PushDate { get; set; }
+        }
+    }
+}
---
+++ src/Monambike.cs
@@ -0,0 +1,37 @@
+﻿namespace Monambike.Data
+{
+    public static class Monambike
+    {
+        public static string Username => "@monambike";
+
+        public static string FirstName => "Vinícius Gabriel";
+
+        public static string SecondName => "Marques de Melo";
+
+        public static DateTime Birthday => new(2002, 11, 24);
+
+        // Personal Website
+        public static readonly Link Website = new("https://monambike.com");
+        // Instagram Accounts
+        public static readonly Link InstagramMonambike = new("https://www.instagram.com/monambike");
+        public static readonly Link InstagramMonambikePortfolio = new("https://www.instagram.com/monambike_portfolio");
+        // Spotify Account
+        public static readonly Link Spotify = new("https://open.spotify.com/user/21jrwvbuag67jrte2nzmplhsy");
+        // Steam Account
+        public static readonly Link Steam = new("https://steamcommunity.com/id/monambike");
+        // YouTube Account
+        public static readonly Link YouTubeMonambike = new("https://www.youtube.com/@monambike");
+        public static readonly Link YouTubeMonambikePortfolio = new("https://www.youtube.com/@monambike_portfolio");
+        public static readonly Link YouTubeMonambikeLive = new("https://www.youtube.com/@monambike_lives");
+        // Linkedin Account
+        public static readonly Link LinkedIn = new("https://www.linkedin.com/in/monambike/");
+        // Code Deploy Account
+        public static readonly Link GitHub = new("https://github.com/monambike");
+        public static readonly Link ItchIo = new("https://monambike.itch.io/");
+
+        public class Link(string url)
+        {
+            public string Url { get; set; } = url;
+        }
+    }
+}
