db8aacb62c2f9336eaffd7bfe67b4102c114850e
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2024-02-16 01:24:53 -0300

    Create unit testing project

---
+++ Monambike.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Monambike.Core.Data;
+using System.Diagnostics;
+
+namespace Monambike.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine(Links.WebsiteLearn);
+        }
+    }
+}
